224c31bdb9f001787c29e5c1a306b4a07162de59
ra155493 <ra155493@C4M13.acad.ucdb.br>
2015-04-29 22:22:42 -0400

    Criação de classes pacote model, exception e PedidoService

---
+++ src/br/com/larimaia/exception/ServiceException.java
@@ -0,0 +1,9 @@
+package br.com.larimaia.exception;
+
+public class ServiceException extends Exception {
+
+    public ServiceException(String msg) {
+        super(msg);
+    }
+    
+}
--- src/br/com/larimaia/service/PedidoService.java
+++ src/br/com/larimaia/service/PedidoService.java
@@ -1,15 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package br.com.larimaia.service;
 
-/**
- *
- * @author ra155493
- */
+import br.com.larimaia.DAO.PedidoDAO;
+import br.com.larimaia.model.Pedido;
+import br.com.larimaia.exception.ServiceException;
+import java.util.List;
+
 public class PedidoService {
-    
+    private PedidoDAO pedidoDAO;
+
+    public PedidoService() {
+        pedidoDAO = new PedidoDAO();
+    }
+
+    public void salvar(Pedido pedido) throws ServiceException {
+
+        if (pedido.getOrigemPedido().isEmpty()) {
+            throw new ServiceException("Campo Local do Contrato é obrigatório!");
+        }
+
+        if (pedido.getDataPedido() == null) {
+            throw new ServiceException("Campo Data do Pedido é obrigatório!");
+        }
+        
+        if (pedido.getCliente().getNome().isEmpty()) {
+            throw new ServiceException("Campo Cliente é obrigatório!");
+        }
+
+        if (pedido.getCerimonial().isEmpty()) {
+            throw new ServiceException("Campo Cerimonial é obrigatório!");
+        }
+        
+        if (pedido.getDataEvento() == null) {
+            throw new ServiceException("Campo Data do Evento é obrigatório!");
+        }
+
+        if (pedido.getTipoEvento().getDescricao().isEmpty()) {
+            throw new ServiceException("Campo Tipo é obrigatório!");
+        }
+        
+        if (pedido.getHoraEvento().isEmpty()) {
+            throw new ServiceException("Campo Hora é obrigatório!");
+        }
+        
+        if (pedido.getLocalEvento().isEmpty()) {
+            throw new ServiceException("Campo Local do Evento é obrigatório!");
+        }
+        
+        if (pedido.getEnderecoEvento().isEmpty()) {
+            throw new ServiceException("Campo Endereço é obrigatório!");
+        }
+        
+        if (pedido.getTipoEvento().getDescricao().isEmpty()) {
+            throw new ServiceException("Campo Tipo é obrigatório!");
+        }
+
+        pedidoDAO.salvar(pedido);
+
+    }
+
+    public void excluir(Integer id) {
+        pedidoDAO.excluir(id);
+    }
+
+    public Pedido buscarPorId(Integer id) {
+        return pedidoDAO.buscarPorId(id);
+    }
+
+    public List<Pedido> buscarTodos() {
+            return pedidoDAO.buscarTodos();
+    }
 }
